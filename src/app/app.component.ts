a5059e60a6f4a15ef985ede2df81a5e5fec5ded7
Mohamed Saeed <mshamoda2003@gmail.com>
2024-05-02 15:36:59 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task1';
+  // title = 'task1';
+  id:number=1
+  imgUrl:string="https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=600"
+  title:string="lapTop"
+  description:string="apple lapTop"
+  price:number=2000
+  btn:string="buy"
+  change(){
+    if(this.btn==="buy"){
+      this.btn="done"
+    }else{
+      this.btn="buy"
+    }
+  }
 }
